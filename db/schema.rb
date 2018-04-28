42687181276d003def6488870d2b4e13692b4056
丸茂沙織 <marumo@marumonoMBP.local>
2018-04-28 23:49:03 +0900

    Mysql2作成 scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325115920) do
+ActiveRecord::Schema.define(version: 20180428141522) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
